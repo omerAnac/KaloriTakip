3b8747717fade2b83fe73afd87ad2d7766213fcf
Ömer Anaç <75081136+omerAnac@users.noreply.github.com>
2022-04-13 23:46:52 +0300

    Add project files.

---
+++ KaloriTakip/UserSet.Designer.cs
@@ -0,0 +1,211 @@
+﻿
+namespace KaloriTakip
+{
+    partial class UserSet
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gbUserSettings = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.txtHeight = new System.Windows.Forms.TextBox();
+            this.txtWeight = new System.Windows.Forms.TextBox();
+            this.dtpBirthDate = new System.Windows.Forms.DateTimePicker();
+            this.lblGender = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Boy = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblUserName = new System.Windows.Forms.Label();
+            this.gbUserSettings.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gbUserSettings
+            // 
+            this.gbUserSettings.Controls.Add(this.label4);
+            this.gbUserSettings.Controls.Add(this.txtPassword);
+            this.gbUserSettings.Controls.Add(this.btnUpdate);
+            this.gbUserSettings.Controls.Add(this.txtHeight);
+            this.gbUserSettings.Controls.Add(this.txtWeight);
+            this.gbUserSettings.Controls.Add(this.dtpBirthDate);
+            this.gbUserSettings.Controls.Add(this.lblGender);
+            this.gbUserSettings.Controls.Add(this.label3);
+            this.gbUserSettings.Controls.Add(this.label2);
+            this.gbUserSettings.Controls.Add(this.Boy);
+            this.gbUserSettings.Controls.Add(this.label1);
+            this.gbUserSettings.Controls.Add(this.lblUserName);
+            this.gbUserSettings.Location = new System.Drawing.Point(56, 12);
+            this.gbUserSettings.Name = "gbUserSettings";
+            this.gbUserSettings.Size = new System.Drawing.Size(383, 409);
+            this.gbUserSettings.TabIndex = 16;
+            this.gbUserSettings.TabStop = false;
+            this.gbUserSettings.Text = "Kullanıcı Bilgileri :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(37, 293);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 17);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Şifre:";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.BackColor = System.Drawing.Color.Bisque;
+            this.txtPassword.Location = new System.Drawing.Point(138, 287);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(164, 22);
+            this.txtPassword.TabIndex = 13;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.BackColor = System.Drawing.Color.Bisque;
+            this.btnUpdate.Location = new System.Drawing.Point(165, 376);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 27);
+            this.btnUpdate.TabIndex = 12;
+            this.btnUpdate.Text = "Değiştir";
+            this.btnUpdate.UseVisualStyleBackColor = false;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // txtHeight
+            // 
+            this.txtHeight.BackColor = System.Drawing.Color.Bisque;
+            this.txtHeight.Location = new System.Drawing.Point(138, 190);
+            this.txtHeight.Name = "txtHeight";
+            this.txtHeight.Size = new System.Drawing.Size(164, 22);
+            this.txtHeight.TabIndex = 10;
+            // 
+            // txtWeight
+            // 
+            this.txtWeight.BackColor = System.Drawing.Color.Bisque;
+            this.txtWeight.Location = new System.Drawing.Point(140, 142);
+            this.txtWeight.Name = "txtWeight";
+            this.txtWeight.Size = new System.Drawing.Size(164, 22);
+            this.txtWeight.TabIndex = 11;
+            // 
+            // dtpBirthDate
+            // 
+            this.dtpBirthDate.CalendarMonthBackground = System.Drawing.Color.Bisque;
+            this.dtpBirthDate.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpBirthDate.Location = new System.Drawing.Point(140, 93);
+            this.dtpBirthDate.Name = "dtpBirthDate";
+            this.dtpBirthDate.Size = new System.Drawing.Size(164, 22);
+            this.dtpBirthDate.TabIndex = 9;
+            // 
+            // lblGender
+            // 
+            this.lblGender.AutoSize = true;
+            this.lblGender.ForeColor = System.Drawing.Color.Bisque;
+            this.lblGender.Location = new System.Drawing.Point(137, 238);
+            this.lblGender.Name = "lblGender";
+            this.lblGender.Size = new System.Drawing.Size(46, 17);
+            this.lblGender.TabIndex = 8;
+            this.lblGender.Text = "label4";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(37, 238);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Cinsiyet:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(37, 142);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Kilo:";
+            // 
+            // Boy
+            // 
+            this.Boy.AutoSize = true;
+            this.Boy.Location = new System.Drawing.Point(37, 190);
+            this.Boy.Name = "Boy";
+            this.Boy.Size = new System.Drawing.Size(36, 17);
+            this.Boy.TabIndex = 3;
+            this.Boy.Text = "Boy:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 99);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Dogum Tarihi:";
+            // 
+            // lblUserName
+            // 
+            this.lblUserName.AutoSize = true;
+            this.lblUserName.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblUserName.Location = new System.Drawing.Point(131, 27);
+            this.lblUserName.Name = "lblUserName";
+            this.lblUserName.Size = new System.Drawing.Size(109, 38);
+            this.lblUserName.TabIndex = 0;
+            this.lblUserName.Text = "label1";
+            // 
+            // UserSet
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(549, 433);
+            this.Controls.Add(this.gbUserSettings);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "UserSet";
+            this.Text = "UserSet";
+            this.Load += new System.EventHandler(this.UserSet_Load_1);
+            this.gbUserSettings.ResumeLayout(false);
+            this.gbUserSettings.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gbUserSettings;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.TextBox txtHeight;
+        private System.Windows.Forms.TextBox txtWeight;
+        private System.Windows.Forms.DateTimePicker dtpBirthDate;
+        private System.Windows.Forms.Label lblGender;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label Boy;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblUserName;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtPassword;
+    }
+}
